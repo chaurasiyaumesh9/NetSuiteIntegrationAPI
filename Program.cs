1ee1a4bffe5061bfae164fe5237bbfeaa3eef750
Umesh C <uchaurasiya@girsoftwareservices.com>
2026-02-21 21:00:54 +0530

    fixing redirection code for production

--- Program.cs
+++ Program.cs
@@ -42,11 +42,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseHttpsRedirection();
 }
 
 app.UseCors("AngularLocal");
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
